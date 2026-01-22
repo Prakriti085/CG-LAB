7d7960fb07ec5f09eca8789e10e4c006584a4f0d
Prakriti085 <PhuyalPrakriti3@gmail.com>
2026-01-22 11:30:16 +0545

    lab2 code

--- LAB2/lab2.cpp
+++ LAB2/lab2.cpp
@@ -1,50 +1,37 @@
-#include <graphics.h>
-#include <math.h>
-#include <stdio.h>
-#include <conio.h>
-
-int main()
-{
+int main() 
+{ 
     int fd = DETECT, gm;
-    int x1, y1, x2, y2, steps, k;
+    int x1, y1, x2, y2, steps, k; 
     float x_incr, y_incr, x, y, dx, dy;
-
-    printf("enter x1 y1 ");
-    scanf("%d %d", &x1, &y1);
-
+    printf("enter x1 y1 "); 
+    scanf("%d %d", &x1, &y1); 
     printf("enter x2 y2 ");
     scanf("%d %d", &x2, &y2);
+    char data[] = "C:\\MinGW\\lib\\libbgi.a"; //static file
 
-    initgraph(&fd, &gm, "");
-
+    initgraph(&fd, &gm, data);
     dx = x2 - x1;
-    dy = y2 - y1;
-
+    dy = y2 - y1; 
     if (abs(dx) > abs(dy))
-    {
-        steps = abs(dx);
-    }
-    else
-    {
-        steps = abs(dy);
-    }
-
+    { 
+        steps = abs(dx); 
+    } 
+    else 
+    { 
+        steps = abs(dy); 
+    } 
     x_incr = dx / steps;
     y_incr = dy / steps;
-
     x = x1;
-    y = y1;
-
+    y = y1; 
     putpixel((int)x, (int)y, WHITE);
-
-    for (k = 1; k <= steps; k++)
-    {
-        x += x_incr;
+    for (k = 1; k <= steps; k++) 
+    { 
+        x += x_incr; 
         y += y_incr;
-        putpixel((int)x, (int)y, WHITE);
+        putpixel((int)x, (int)y, WHITE); 
     }
-
-    getch();
+    getch(); 
     closegraph();
-    return 0;
+    return 0; 
 }
